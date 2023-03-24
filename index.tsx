8d91e8aa59514feffbfbebe16fc504159ee9fdc2
dennishooo <hkhdennis@gmail.com>
2023-03-24 16:33:00 +0800

    Add hook

--- index.tsx
+++ index.tsx
@@ -28,16 +28,57 @@ function render(reactElememt: any, container: HTMLElement) {
   container.appendChild(element);
 }
 
-const App = () => (
-  <div className="container" id="container">
-    <h1 className="title">wtf</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus
-      modi, sit cumque accusantium nemo debitis molestias ipsum voluptate
-      commodi fuga expedita, magnam, et architecto! Aut similique cumque ex rem
-      quibusdam.
-    </p>
-  </div>
-);
+const state = [];
 
+function useState(initialState: any): [any, any] {
+  let state = initialState;
+  console.log({ initialState });
+
+  let setState = (newState: any) => {
+    state = newState;
+    console.log({ newState });
+    console.log({ state });
+  };
+  return [state, setState];
+}
+
+const App = () => {
+  const [person, setPerson] = useState("dennis");
+  const [count, setCount] = useState(0);
+
+  let onNameChange = (name) => {
+    reRender();
+    console.log(name);
+
+    setPerson(name);
+  };
+
+  return (
+    <div className="container" id="container">
+      <h1 className="title">wtf {person}</h1>
+      <h3 className="title">the count is: {count}</h3>
+
+      <input
+        type="text"
+        placeholder="name"
+        onchange={(e) => {
+          console.log("changing......");
+
+          onNameChange(e.target.value);
+        }}
+      />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus
+        modi, sit cumque accusantium nemo debitis molestias ipsum voluptate
+        commodi fuga expedita, magnam, et architecto! Aut similique cumque ex
+        rem quibusdam.
+      </p>
+    </div>
+  );
+};
 render(<App />, document.querySelector("#app"));
+
+const reRender = () => {
+  document.querySelector("#app").innerHTML = "";
+  render(<App />, document.querySelector("#app"));
+};
